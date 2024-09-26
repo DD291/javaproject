cfe49dd5d7140e6dd0e00f6f0c77db03f6738ab3
DD291 <dda291@yandex.ru>
2024-09-26 18:11:24 +0900

    one require per file

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ## Getting Started
 The folders to deal with are inside the folder "Folder" in the same directory, as the file
+First find the file that does not require anything
+Then use bubble sort to sort
--- src/App.java
+++ src/App.java
@@ -1,8 +1,13 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -11,25 +16,92 @@ public class App {
         System.out.println("PATH: " + dirname);
         File folder = new File(dirname);
         System.out.println("folder: " + folder);
-//         File folder = new File("/Users/you/folder/");
-        File[] listOfFiles = folder.listFiles();
 
-        for (File file : listOfFiles) {
-            if (file.isFile()) {
-                System.out.println(file.getName());
+        Path rootPath = Paths.get(System.getProperty("user.dir"), "Folder");
+        List<Path> allFiles = new ArrayList<>();
+        listAllFiles(rootPath, allFiles);
+        System.out.println("Found files:");
+
+        // first: find file with no require - if not exists, impossible case 
+        //second : bubble sort : 
+        // start from second element :every time if (this file(n) has "require file(n+1)" - swap) else go to next
+        String[] filesDirsArray = new String[allFiles.size()];
+        for(int i = 0; i < allFiles.size(); i++) filesDirsArray[i] = allFiles.get(i).toString();
+        
+        for (int i = 0; i < filesDirsArray.length; i++)
+        {
+            System.out.println("forliio" + filesDirsArray[i] + " ");
+            // File file = new File("C:/Users/dda29/Documents/GitHub/javaproject/Folder/Folder 1/File 1-1.txt");
+            File file = new File(filesDirsArray[i]);
+
+            try {
+                Scanner scanner = new Scanner(file);
+                int lineNum = 0;
+                boolean foundRequire = false;
+                while (scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    lineNum++;
+                    if(line.contains("require")) { 
+                        System.out.println("line num: " +lineNum );
+                        System.out.println("naiyou:  " +line ); 
+                        foundRequire = true;
+                    }
+                }
+                if (foundRequire==false){
+                    //swap with first
+                    String temp = filesDirsArray[i];
+                    filesDirsArray[i] = filesDirsArray[0];
+                    filesDirsArray[0] = temp;
+                }
+                scanner.close();
+            } catch(FileNotFoundException e) { 
             }
         }
 
-        List<String> dirStack = new Stack<String>();
+        for(int i = 0; i < filesDirsArray.length; i++){ 
+            System.out.println("allFiles:  "+ i + ": " + filesDirsArray[i] ); 
+        };
+
+            
 
 
-        String [] listOfFolderes = folder.list();
+    }
 
-        for (String file : listOfFolderes) {
-                System.out.println(file);
-        
+     private static void listAllFiles(Path currentPath, List<Path> allFiles)
+      throws IOException 
+    {
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(currentPath)) 
+        {
+            for (Path entry : stream) {
+                if (Files.isDirectory(entry)) {
+                    listAllFiles(entry, allFiles);
+                } else {
+                    allFiles.add(entry);
+                }
+            }
         }
+    }
 
+
+    static void bubbleSort(int arr[], int n){
+
+        int i, j, temp;
+        boolean swapped;
+        for (i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    
+                    // Swap arr[j] and arr[j+1]
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (swapped == false)
+                break;
+        }
     }
 
    
